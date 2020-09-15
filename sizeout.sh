2e57725c6e99cf1c9d7f67a67b29570fcc615224
bc8c <bc8c@naver.com>
2020-09-15 23:04:05 +0900

    first commit and make sizeout.sh

---
+++ sizeout.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+if [ $# -ne 1 ];then
+  echo "#usage: sh $0 <file>"
+  exit
+fi
+ 
+FILE=$1
+set $(wc -c "$FILE")
+FILESIZE=$1
+#FILESIZE=$(stat -c%s "$FILE")
+echo "file size: $FILESIZE"
+if [ $FILESIZE -lt 1000 ];then
+  #command goes here
+  echo "file size is less than 1000"
+fi
